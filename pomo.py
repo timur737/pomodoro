3628935e23024a540de7dd21fa419591da7590d9
Timur <ulanovnurdan@gmail.com>
2021-01-30 00:25:45 +0600

    Some change

--- pomo.py
+++ pomo.py
@@ -5,6 +5,7 @@ from tkinter import messagebox
 temp = 1500
 after_id = ""
 
+
 def report():
     global temp, after_id
     after_id = root.after(1000, report)
@@ -15,9 +16,12 @@ def report():
         end_pm()
         messagebox.showinfo(message="Иди отдохни")
         relax()
-        
+
+
 tmp = 300
 aft = ''
+
+
 def relax():
     global tmp, aft
     aft = root.after(1000, relax)
@@ -29,15 +33,20 @@ def relax():
         messagebox.showinfo(message="За работу!")
         res_pm()
 
+
 def start_pm():
     btn1.grid_forget()
     btn2.grid(row=1, column=0, sticky="ew")
     btn3.grid(row=1, column=1, sticky="ew")
     report()
+
+
 def end_pm():
     btn1.grid(row=1, column=0, sticky="ew")
     btn3.grid(row=1, column=1, sticky="ew")
     root.after_cancel(after_id)
+
+
 def res_pm():
     global temp
     temp = 1500
@@ -46,11 +55,11 @@ def res_pm():
     btn3.grid_forget()
     end_pm()
     btn1.grid(row=1, column=0)
+
+
 root = Tk()
 
 root.title("Technique POMODORO")
-
-
 
 label1 = Label(root, width=5, font=("Ubuntu", 100), text="25:00")
 label1.grid(row=0, columnspan=2)
@@ -62,4 +71,3 @@ btn3 = Button(text="RESET", font=("Ubuntu", 30), command=res_pm)
 btn1.grid(row=1, columnspan=2, sticky="ew")
 
 root.mainloop()
-
